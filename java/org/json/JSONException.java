74900dbceb0629c5a9ff966902fc28f6bfd92c11
alex.fiennes <alex@fiennes.org>
2011-02-22 16:03:17 +0000

    don't store the cause in the implementation but just forward it up to the superclass

--- java/org/json/JSONException.java
+++ java/org/json/JSONException.java
@@ -2,28 +2,37 @@ package org.json;
 
 /**
  * The JSONException is thrown by the JSON.org classes when things are amiss.
+ * 
  * @author JSON.org
  * @version 2010-12-24
  */
-public class JSONException extends Exception {
-	private static final long serialVersionUID = 0;
-	private Throwable cause;
+public class JSONException
+  extends Exception
+{
+  private static final long serialVersionUID = 0;
 
-    /**
-     * Constructs a JSONException with an explanatory message.
-     * @param message Detail about the reason for the exception.
-     */
-    public JSONException(String message) {
-        super(message);
-    }
+  /**
+   * Constructs a JSONException with an explanatory message.
+   * 
+   * @param message
+   *          Detail about the reason for the exception.
+   */
+  public JSONException(String message)
+  {
+    super(message);
+  }
 
-    public JSONException(Throwable cause) {
-        super(cause.getMessage());
-        this.cause = cause;
-    }
+  public JSONException(String message,
+                       Throwable cause)
+  {
+    super(message,
+          cause);
+  }
 
-    @Override
-    public Throwable getCause() {
-        return this.cause;
-    }
+  public JSONException(Throwable cause)
+  {
+    super(cause.getMessage(),
+          cause);
+  }
+
 }
