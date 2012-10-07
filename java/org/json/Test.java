c9dcb6f1be2f7cc284c4395cb062a5d04574c1ee
alex.fiennes <alex@fiennes.org>
2012-10-07 13:51:54 +0100

    remove redundant casts

--- java/org/json/Test.java
+++ java/org/json/Test.java
@@ -1263,7 +1263,7 @@ public class Test extends TestCase {
 
         iterator = jsonobject.keys();
         while (iterator.hasNext()) {
-            string = (String) iterator.next();
+      string = iterator.next();
             assertTrue(expectedKeys.remove(string));
         }
         assertEquals(0, expectedKeys.size());
