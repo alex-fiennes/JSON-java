47210d5d6b4eade46de72b5176fe75257c44c03a
Alex Fiennes <alex@fiennes.org>
2019-08-10 23:43:50 +0100

    remove unused code

--- java/org/json/JSONTokenerReader.java
+++ java/org/json/JSONTokenerReader.java
@@ -156,25 +156,6 @@ public class JSONTokenerReader<A extends JSONArray, O extends JSONObject>
   }
 
   /**
-   * Consume the next character, and check that it matches a specified character.
-   * 
-   * @param c
-   *          The character to match.
-   * @return The character.
-   * @throws JSONException
-   *           if the character does not match.
-   */
-  public char next(char c)
-      throws JSONException
-  {
-    char n = next();
-    if (n != c) {
-      throw syntaxError("Expected '" + c + "' and instead saw '" + n + "'");
-    }
-    return n;
-  }
-
-  /**
    * Get the next n characters.
    *
    * @param n
